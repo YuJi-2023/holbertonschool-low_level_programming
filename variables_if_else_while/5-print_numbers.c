1f1653fdc7e2fb3bed12b7ce2e8fe593340a29a9
cohort20 <sophia.jiyu@gmail.com>
2023-02-19 21:18:54 -0800

    print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+	printf("%d", a);
+	a++;
+	}
+	putchar('\n');
+	return (0);
+}
