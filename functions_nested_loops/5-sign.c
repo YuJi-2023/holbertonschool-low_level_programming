49f162e0e1f6839f980d20cd6efa26e502c5d2c4
cohort20 <sophia.jiyu@gmail.com>
2023-02-22 14:52:29 -0800

    print_sign: print the sign of a number

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _print_sign - print sign for a number
+ * @n: accept intiger input
+ * Return: 1 (n > 0);
+ *         0 (n == 0);
+ *        -1 (n < 0);
+ */
+int print_sign(int n)
+{
+	int num = n;
+
+	if (num > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (num == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
