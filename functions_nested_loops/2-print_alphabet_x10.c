8689b215a52ef74f937f38a3ef3aa4dd8bb62424
cohort20 <sophia.jiyu@gmail.com>
2023-02-22 02:04:38 -0800

    update comment

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - writes alphabet in lowercase 10 times
+ * print_alphabet_x10 - writes alphabet in lowercase 10 times
  */
 void print_alphabet_x10(void)
 {
