b1c9eb0555d1f350826ac660c242f2b8497e6535
cohort20 <sophia.jiyu@gmail.com>
2023-02-23 22:41:08 -0800

    the fizz buzz test %3 %5

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to print numbers 1 to 100
+ * n%3=0 - print Fizz;
+ * n%5=0 - print Buzz;
+ * n%3=0 && n%5=0 - print fizzbuzz
+ * @n:the inpurt parameter integer
+ * Return: the value of int n
+ */
+int main(int n)
+{
+	n = 1;
+
+	while (n <= 100)
+	{
+		int i = n % 3;
+		int j = n % 5;
+
+		if (i == 0)
+		{
+			printf("%s ", "Fizz");
+		}
+		if (j == 0)
+		{
+			printf("%s ", "Buzz");
+		}
+		if (i != 0 && j != 0)
+		{
+			printf("%d ", n);
+		}
+		n = n + 1;
+	}
+	putchar('\n');
+}
