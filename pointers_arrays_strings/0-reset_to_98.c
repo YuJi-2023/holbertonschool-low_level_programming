5d99fcf2a94f8980507ff1ef716f0d25abdf90fd
cohort20 <sophia.jiyu@gmail.com>
2023-02-26 18:29:56 -0800

    change for betty documentation style

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * reset_to_98 - take a pointer to an int and update the value to 98
- * Return:
+ * @p: takes the input parameter (address of the int)
+ * Return
  */
 void reset_to_98(int *p)
 {
