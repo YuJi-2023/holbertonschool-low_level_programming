615f830bdd0d3f365861821e6303bdd34ce905e1
cohort20 <sophia.jiyu@gmail.com>
2023-03-05 05:17:29 -0800

    update code

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagsums - add and print the sum of two diagonal of integers 
+ * print_diagsums - add and print the sum of two diagonal of integers
  * @a: pointer to a  two dimensional array
  * @size: size of the matrix of the integers
  * Return: no need
@@ -15,8 +15,9 @@ void print_diagsums(int *a, int size)
 
 	while (i < size)
 	{
-		left = left + a[i * (size + 1)];
-		right = right + a[(size - 1) * (i + 1)];	
+		left = left + a[i * size + i];
+		right = right + a[(size - 1) + (size - 1) * i];
+		i = i + 1;
 	}
 	printf("%d, %d\n", left, right);
 }
