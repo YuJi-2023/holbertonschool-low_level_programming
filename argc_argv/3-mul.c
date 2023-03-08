aa081c1e97e1553eb90f0e4a64b76037c5cedca3
cohort20 <sophia.jiyu@gmail.com>
2023-03-08 03:25:21 -0800

    print the result of multiplication of two numbers

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+# include "main.h"
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+
+/**
+ * main - Write a program that multiplies two numbers
+ * @argc: count of total command line arguments
+ * @argv: the array of character string of each command line argument
+ * atoi: to convert string to int
+ * Return: 0 success; if argc != 2, return 1 
+ */
+int main(int argc, char *argv[])
+{
+	int sum;
+	int i;
+	int j;
+
+	if (argc == 3)
+	{
+		i = atoi(argv[1]);
+		j = atoi(argv[2]);
+		sum = i * j;
+		printf("%d\n", sum);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (1);
+}
