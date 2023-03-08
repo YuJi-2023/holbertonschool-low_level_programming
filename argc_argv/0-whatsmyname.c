a08077711ad5ff6c39e1de9a4e1b0f0855205e5d
cohort20 <sophia.jiyu@gmail.com>
2023-03-07 18:49:02 -0800

    update main.h; 0-whatsmyname.c to print the program name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+# include "main.h"
+# include <stdio.h>
+/**
+ * main - Write a program that prints its name, followed by a new line
+ * @argc: count of total command line arguments
+ * @argv[]: the array of character string of each command line argument
+ * return: 0 success
+ */
+int main (int argc, char *argv[])
+{
+	if (argc >= 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
