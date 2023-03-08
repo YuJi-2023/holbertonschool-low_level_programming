6e4b19c8cb4a1bc61fae29b309f56fc30d971c32
cohort20 <sophia.jiyu@gmail.com>
2023-03-07 18:51:28 -0800

    update code

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,10 +3,10 @@
 /**
  * main - Write a program that prints its name, followed by a new line
  * @argc: count of total command line arguments
- * @argv[]: the array of character string of each command line argument
+ * @argv: the array of character string of each command line argument
  * return: 0 success
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc >= 1)
 	{
