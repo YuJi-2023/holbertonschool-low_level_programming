7f54f59ee852f26f2141ffd3baad1b356c7a751a
cohort20 <sophia.jiyu@gmail.com>
2023-03-08 16:02:25 -0800

    write a program that adds positive numbers

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -5,10 +5,32 @@
 # include <stdlib.h>
 
 /**
+ * check_argv_digit - check each digit of argv[] for number or not
+ * @digits: input parameter
+ * Return: 0 if is number; otherwise 1
+ */
+int check_argv_digit(char *digits)
+{
+	int n;
+
+	n = 0;
+	while (digits[n] != '\0')
+	{
+		if (isdigit(digits[n]) == 0)
+		{
+			return (1);
+		}
+		n = n + 1;
+	}
+	return (0);
+}
+
+/**
  * main - Write a program that adds positive numbers
  * @argc: count of total command line arguments
  * @argv: the array of character string of each command line argument
  * atoi: to convert string into integer
+ * isdigit: to check if argv[] is integer
  * Return: 0 success; if argv[] is not digits, return 1
  */
 int main(int argc, char *argv[])
@@ -23,9 +45,10 @@ int main(int argc, char *argv[])
 	}
 
 	count = 1;
-	while (argc > 1 && count < argc)
+	add = 0;
+	while (count < argc)
 	{
-		if (isdigit(*argv[count]) == 0)
+		if (check_argv_digit(argv[count]) == 1)
 		{
 			printf("Error\n");
 			return (1);
