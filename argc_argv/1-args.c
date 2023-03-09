2ec5880f3e14b329bbbc62185334a33f92d8fdcd
cohort20 <sophia.jiyu@gmail.com>
2023-03-08 19:13:24 -0800

    update code for unused parameter argv

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,21 +1,14 @@
 # include "main.h"
 # include <stdio.h>
 /**
- * main - Write a program that prints its name, followed by a new line
+ * main - Write a program that prints the number of arguments passed into
  * @argc: count of total command line arguments
  * @argv: the array of character string of each command line argument
  * Return: 0 success
  */
 int main(int argc, char *argv[])
 {
-	int i;
-
-	i = 0;
-	while (i < argc)
-	{
-		argv[i] = argv[i + 1];
-		i = i + 1;
-	}
-	printf("%d\n", i - 1);
+	(void)argv;
+	printf("%d\n", argc - 1);
 	return (0);
 }
