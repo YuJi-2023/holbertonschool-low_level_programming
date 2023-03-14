6417786119928d64475678d65c99cc025aed1b12
cohort20 <sophia.jiyu@gmail.com>
2023-03-14 03:17:58 -0700

    update code

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -28,6 +28,15 @@ char *str_concat(char *s1, char *s2)
 	int i;
 	char *result;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
 	result = malloc(sizeof(*result) * (_strlen(s1) + _strlen(s2) + 1));
 	if (result == NULL)
 	{
