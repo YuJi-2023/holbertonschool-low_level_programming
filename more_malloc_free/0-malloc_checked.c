24d7d8b9c87fceb6b3d2b98d314bb8658e6565d7
cohort20 <sophia.jiyu@gmail.com>
2023-03-14 17:54:36 -0700

    update code

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -3,7 +3,7 @@
 /**
  * malloc_checked - allocates memory using malloc
  * @b: input size of memory need to be allocated
- * Return: pointer to the memory
+ * Return: pointer to the memory or status valule of 98 if fails
  */
 void *malloc_checked(unsigned int b)
 {
@@ -14,9 +14,7 @@ void *malloc_checked(unsigned int b)
 	mem = malloc(n);
 	if (mem == NULL)
 	{
-		return (NULL);
-		_putchar(98);
+		exit(98);
 	}
 	return (mem);
-	free(mem);
 }
