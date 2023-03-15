e8114b807b4c88ef6423508e21c5b78e55865adc
cohort20 <sophia.jiyu@gmail.com>
2023-03-14 20:49:49 -0700

    write own _calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - using malloc to create own calloc funtion
+ *@nmemb: input number of elements of the array
+ *@size: input of each element's byte size
+ *Return: pointer to allocated memory or NULL if fails
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	void *ptr;
+	unsigned int total;
+
+	total = nmemb * size;
+	ptr = malloc(total);
+	if (ptr == NULL || total == 0)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < size)
+	{
+		*((char *)ptr + i) = 0;
+		i = i + 1;
+	}
+	return (ptr);
+}
