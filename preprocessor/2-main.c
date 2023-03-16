b69c018faf6cc7401f04939ffb728c45a59400b2
cohort20 <sophia.jiyu@gmail.com>
2023-03-16 03:12:27 -0700

    betty style update

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
- * filename - to print out the name of the file it was complied from
+ * main - to print out the name of the file it was complied from
  * Return: 0 on sucess
  */
-int main()
+int main(void)
 {
 	char *name;
 	int i;
 
 	name = __FILE__;
 	i = 0;
-	while (name[i] != '\0') 
-	
+	while (name[i] != '\0')
+
 	{
 		_putchar(name[i]);
 		i = i + 1;
 	}
 	_putchar('\n');
-	return(0);
+	return (0);
 }
