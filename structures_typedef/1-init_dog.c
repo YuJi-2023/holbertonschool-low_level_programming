64db7a0b2ff8d80fddc7c4ffc5b461562536e3f9
cohort20 <sophia.jiyu@gmail.com>
2023-03-19 18:31:50 -0700

    remove return in initialize file as no need

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -8,12 +8,11 @@
  * @name: dog name
  * @age: dog age
  * @owner: dog owner
- * Return: void
+ * Return: no need
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-	return (void);
 }
