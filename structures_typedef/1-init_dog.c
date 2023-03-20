3c546afa02e195e4f30f23c155d77a7016c5850f
cohort20 <sophia.jiyu@gmail.com>
2023-03-19 18:28:24 -0700

    update header file and initialize struct dog

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - initialize the datas for the dog
+ * @d: struct name
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	return (void);
+}
