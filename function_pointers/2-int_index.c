dc2f3440bdb16c05f4643194d8e5da1a3323d539
cohort20 <sophia.jiyu@gmail.com>
2023-03-20 20:14:34 -0700

    update header file; create a function using function pointer to search for matching array element and return its index

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,34 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * int_index - a function that search the first matching integer and
+ * returns its index from an input int array
+ * @array: pointer to an array parameter
+ * @size: size of the input array
+ * @cmp: function poiter that takes the input function
+ * Return: index to the array element that matches
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+
+	i = 0;
+	if (size > 0)
+	{
+		while (i < size)
+		{
+			while ((*cmp)(array[i]) == 0)
+			{
+				i = i + 1;
+			}
+			return (i);
+		}
+		return (-1);
+	}
+	return (-1);
+}
