ef2fa5d882722a5c9e3274ec7b45bdacef553612
cohort20 <sophia.jiyu@gmail.com>
2023-03-20 17:43:17 -0700

    betty style for function pointer to print name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,10 +2,9 @@
 #include <stdlib.h>
 /**
  * print_name - a function that prints a name
- * @name - pointer to input parameter of names
- * @(*f) - function pointer points to functions that take
+ * @name: pointer to input parameter of names
+ * @f: function pointer points to functions that take
  * in char * parameter and return void
- * 
  * Return: no return
  */
 void print_name(char *name, void (*f)(char *))
