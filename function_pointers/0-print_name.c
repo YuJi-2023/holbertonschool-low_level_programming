218124fd36c55933f424af0883938cc48db45bae
cohort20 <sophia.jiyu@gmail.com>
2023-03-20 17:40:43 -0700

    function pointers; add header file; add README;

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * print_name - a function that prints a name
+ * @name - pointer to input parameter of names
+ * @(*f) - function pointer points to functions that take
+ * in char * parameter and return void
+ * 
+ * Return: no return
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	(*f)(name);
+}
