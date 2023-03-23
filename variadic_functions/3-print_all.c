dfc2d68eb269d0e1017ebc80855ef37951fb5bfe
cohort20 <sophia.jiyu@gmail.com>
2023-03-23 05:09:45 -0700

    update header file and including new struct; create print all function

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,89 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_char - print char -promoted type to int
+ * @i: input paramter
+ * Return: 0
+ */
+int print_char(va_list i)
+{
+	printf("%c", va_arg(i, int));
+	return (0);
+}
+/**
+ * print_int - print int
+ * @i: input paramter
+ * Return: 0
+ */
+int print_int(va_list i)
+{
+	printf("%d", va_arg(i, int));
+	return (0);
+}
+/**
+ * print_float - print float, promoted type to double
+ * @i: input paramter
+ * Return: 0
+ */
+int print_float(va_list i)
+{
+	printf("%f", va_arg(i, double));
+	return (0);
+}
+/**
+ * print_string - print string, treat NULL as (nil)
+ * @i: input paramter
+ * Return: 0
+ */
+int print_string(va_list i)
+{
+	char *s;
+
+	s = va_arg(i, char *);
+	if (s == NULL)
+	{
+		printf("(nil)");
+		return (0);
+	}
+	printf("%s", s);
+	return (0);
+}
+
+/**
+ * print_all - a function that prints all inputs
+ * @format: a list of types of arguments passed in
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	int count;
+	int i;
+	va_list arg;
+	print_type ops[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+
+	va_start(arg, format);
+	count = 0;
+	while (format[count] != '\0' && format != NULL)
+	{
+		i = 0;
+		while (ops[i].f != NULL)
+		{
+			if (format[count] == *(ops[i].type))
+			{
+				ops[i].f(arg);
+				printf(", ");
+			}
+			i = i + 1;
+		}
+		count = count + 1;
+	}
+	printf("\n");
+	va_end(arg);
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,27 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
-
+#include <stdarg.h>
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+/**
+ * struct type - Struct type which includes a char * and a function pointer
+ *
+ * @type: pointer to data types
+ * @f: The function associated to print each data type
+ */
+typedef struct type
+{
+	char *type;
+	int (*f)(va_list i);
+} print_type;
+
+int print_char(va_list i);
+int print_int(va_list i);
+int print_float(va_list i);
+int print_string(va_list i);
+
 void print_all(const char * const format, ...);
 #endif
