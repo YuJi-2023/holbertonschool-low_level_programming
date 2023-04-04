7f765752d1b572a8dcf4cf9cca94523204566b71
cohort20 <sophia.jiyu@gmail.com>
2023-04-03 19:11:19 -0700

    update function 0 file to apply betty style

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -2,13 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * print_dlistint - a function that prints all the elements of a double linked list
+ * print_dlistint - a function that prints
+ * all the elements of a double linked list
  * @h: input parameter of a pointer to a list
  * Return: the number of nodes in the list
  */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count;
+
 	count = 0;
 	while (h != NULL)
 	{
