069ff85af888222ad81ecbab4f9ace2f03c12e9d
cohort20 <sophia.jiyu@gmail.com>
2023-04-04 04:25:46 -0700

    create a function that returns the sum of all data value (n) of a dlistint_t list

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * sum_dlistint - a function that returns the sum of all the data value(n)
+ * in dlistint_t list
+ * @head: input parameter of a pointer to a list
+ * Return: the sum result or 0 if list is empty
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+	dlistint_t *ptr;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	ptr = head;
+	sum = 0;
+	while (ptr->next != NULL)
+	{
+		ptr = ptr->next;
+		sum = sum + ptr->n;
+	}
+	return (sum);
+}
