beb814649e8649c72f60798804b66dbffce83d58
cohort20 <sophia.jiyu@gmail.com>
2023-04-04 05:54:41 -0700

    create a function that insert a new node at the given index position to a dlistint_t list

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,73 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * dlistint_len - a function that returns the number of elements in
+ * in a double linked list
+ * @h: input parameter of a pointer to a list
+ * Return: the number of elements in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		count = count + 1;
+	}
+	return (count);
+}
+
+/**
+ * insert_dnodeint_at_index - a function that inserts a new node to the nth
+ * position of a dlistint_t list
+ * @h: input parameter of a pointer to a pointer to
+ * the beginning of a double linked list
+ * @idx: input parameter of the number nth, starting from 0
+ * @n: input data value to be added as the new node
+ * Return: return the address of the new node
+ * or NULL if it fails;
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	size_t len;
+	dlistint_t *ptr;
+	dlistint_t *ptr2;
+	dlistint_t *newp;
+
+	newp = malloc(sizeof(dlistint_t));
+	if (newp == NULL)
+	{
+		return (NULL);
+	}
+	len = dlistint_len(*h);
+	if (idx >= len)
+	{
+		return (NULL);
+	}
+	ptr = *h;
+	while (idx != 1)
+	{
+		ptr = ptr->next;
+		idx = idx - 1;
+	}
+	if (ptr->next == NULL)
+	{
+		ptr->next = newp;
+		newp->prev = ptr;
+	}
+	else
+	{
+		ptr2 = ptr->next;
+		newp->n = n;
+		ptr->next = newp;
+		ptr2->prev = newp;
+		newp->prev = ptr;
+		newp->next = ptr2;
+	}
+	return (newp);
+}
